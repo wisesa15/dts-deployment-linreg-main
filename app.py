94a4f5942647ab16b0528fb5bca1fa5261b5ee12
wisesa15 <priyo.tw@gmail.com>
2021-12-12 22:28:56 +0700

    Menambahkan tampilan awal dan tampilan prediksi untuk lstm dan linear regression

--- app.py
+++ app.py
@@ -6,30 +6,39 @@ import pickle
 
 app = Flask(__name__)
 
-# model_file = open('LinearRegressionPDRB.pkl', 'rb')
-# model = pickle.load(model_file, encoding='bytes')
-model = tf.keras.models.load_model('model_lstm.h5')
+model_file = open('LinearRegressionPDRB.pkl', 'rb')
+model_linreg = pickle.load(model_file, encoding='bytes')
+model_lstm = tf.keras.models.load_model('model_lstm.h5')
 scaller_file = open('scaller.pkl', 'rb')
 scaller = pickle.load(scaller_file, encoding='bytes')
 
 @app.route('/')
 def index():
+    return render_template('index0.html')
+
+@app.route('/linreg')
+def linreg():
+    return render_template('index.html', pdrb=0)
+
+@app.route('/linreg', methods=['POST'])
+def linregPredict():
+    '''
+    Predict the insurance cost based on user inputs
+    and render the result to the html page
+    '''
+    tahun = int(request.form.get('Tahun'))
+    
+    prediction = model_linreg.predict([[tahun]])
+    output = prediction[0]
+
+    return render_template('index.html', pdrb=output, tahun=tahun)
+
+@app.route('/lstm')
+def lstm():
     return render_template('index1.html', pdrb=0)
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     '''
-#     Predict the insurance cost based on user inputs
-#     and render the result to the html page
-#     '''
-#     tahun = int(request.form.get('Tahun'))
-    
-#     prediction = model.predict([[tahun]])
-#     output = prediction[0]
-
-#     return render_template('index.html', pdrb=output, tahun=tahun)
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/lstm', methods=['POST'])
+def lstmPredict():
     '''
     Predict the insurance cost based on user inputs
     and render the result to the html page
@@ -40,7 +49,7 @@ def predict():
     data = scaller.transform(data)
     data = np.reshape(data, (1,30,1))
     
-    prediction = model.predict(data)
+    prediction = model_lstm.predict(data)
     output = prediction
     output = scaller.inverse_transform(output)[0][0]
 
